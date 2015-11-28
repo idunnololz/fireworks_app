2211c3d9f9b6ca0100271d6cd89289f805719c5e
idunnololz <garyguo9@gmail.com>
2015-11-28 18:41:15 -0500

    Fix heroku server sleeping.

--- fireworks-client/src/js/index.js
+++ fireworks-client/src/js/index.js
@@ -29,16 +29,10 @@ require(['app/consts', 'jquery', 'React', 'libs/socket.io', 'app/game_room', 'li
     if (Consts.PROD) {
         // heroku keep alive
         setInterval(function() {
-            $.get("https://murmuring-mountain-5923.herokuapp.com/", function(data) {
+            $.get("https://murmuring-mountain-5923.herokuapp.com/ping", function(data) {
                 console.log("result: " + data);
             });
         }, 300000); // every 5 minutes (300000)
-    } else {
-        setInterval(function() {
-            $.get("http://localhost:3000/ping/", function(data) {
-                console.log("result: " + data);
-            });
-        }, 3000); // every 5 minutes (300000)
     }
 
     var GameUi = React.createClass({displayName: "GameUi",
